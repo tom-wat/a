15d750e79bfd352df4a675e773ce704b1dc002dd
tom-wat <52376398+tom-wat@users.noreply.github.com>
2020-06-25 14:15:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var textAnim = () => {
+  var c3 = document.querySelector('.c3');
+  var c4 = document.querySelector('.c4');
+  var c3Position = c3.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1.2;
+  console.log(c3, c4, c3Position, screenPosition);
+  if (c3Position < screenPosition) {
+    c3.classList.add('text-appear');
+    c4.classList.add('text-appear');
+  } else {
+    c3.classList.remove('text-appear');
+    c4.classList.remove('text-appear');
+  };
+};
+
+window.addEventListener('scroll', textAnim);
